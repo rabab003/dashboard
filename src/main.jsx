09f8928bb0264d2a53af7f9d1d0de7c1624f2a61
rabab003 <bintaliraq4@gmail.com>
2025-06-12 22:39:57 +0300

    10:40 PM

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDOM from "react-dom/client"
 import App from './App.jsx'
+import React from 'react'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import './index.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom"
+import { Dashboard } from '@mui/icons-material'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      {/* <Route path='dashboard' element={<Dashboard/>}/> */}
+    </Route>
+  )
+)
+
+// @ts-ignore
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
 )
