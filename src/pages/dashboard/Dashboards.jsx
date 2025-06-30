62dc1184c40f2c1afba420097aab2b18a964ef2c
rabab003 <bintaliraq4@gmail.com>
2025-06-30 16:17:57 +0300

    4:17 PM

--- src/pages/dashboard/Dashboards.jsx
+++ src/pages/dashboard/Dashboards.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import Row1 from "./Rows/Row1";
+import { Box, Button } from "@mui/material";
+import { DownloadDoneOutlined } from "@mui/icons-material";
 
 export default function Dashboards() {
-  return <div>dashboard page</div>;
+  return (
+    <>
+      <Box sx={{ textAlign: "right" }}>
+        <Button variant="contained" color="primary">
+          <DownloadDoneOutlined />
+          Donwload Reports
+        </Button>
+      </Box>
+      dashboard
+      <Row1 />
+    </>
+  );
 }
